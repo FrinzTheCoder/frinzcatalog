9a430da58a2982d9e45b6cd0516d618cdedff5b9
Falensi Azmi <falensiazmi@gmail.com>
2024-01-16 14:29:33 +0700

    fix: add csrf trusted origin

--- frinzcatalog/settings.py
+++ frinzcatalog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_8$ee5tot&pctsv2jprys#!l^5f%!y^ht#^f0x7k(fvh4xj*h@
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = ['https://frinzcatalog.falensiazmi.net']
 
 # Application definition
 
