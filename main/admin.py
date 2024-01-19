a451971e707cdf1fc3c98790628e1709f3461534
Falensi Azmi <falensiazmi@gmail.com>
2024-01-19 16:52:20 +0700

    fix: fix some migrations and models stuff

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from main.models import Content, Tag
+from main.models import Content, Tag, Like, Comment
 
 admin.site.register(Content)
 admin.site.register(Tag)
+admin.site.register(Like)
+admin.site.register(Comment)
