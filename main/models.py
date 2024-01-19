ac48fe01d224a55046df37bcdc3a9f26d14b5268
Falensi Azmi <falensiazmi@gmail.com>
2024-01-19 16:32:18 +0700

    chore: add like and comment model

--- main/models.py
+++ main/models.py
@@ -14,7 +14,12 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+class Like(models.Model):
+    ip_address = models.CharField(max_length=15)
+
+class Comment(models.Model):
+    comment = models.TextField(default='')
 
 class Content(models.Model):
     category = models.CharField(
@@ -22,12 +27,14 @@ class Content(models.Model):
         choices=[(tag.name, tag.value) for tag in Category],
         default=Category.RANDOM
     )
-    name = models.CharField(default='', max_length=255)
-    latin_name = models.CharField(default='', max_length=255)
+    name = models.CharField(max_length=255)
+    latin_name = models.CharField(default='Unknown', max_length=255)
     resource = models.URLField()
-    location_found = models.CharField(default='', max_length=255)
-    description = models.TextField()
-    tags = models.ManyToManyField(Tag)
+    location_found = models.CharField(default='Unknown', max_length=255)
+    description = models.TextField(default='')
+    tags = models.ManyToManyField(Tag, blank=True)
+    likes = models.ManyToManyField(Like, blank=True)
+    comment = models.ManyToManyField(Comment, blank=True)
 
     def __str__(self):
         return self.name
