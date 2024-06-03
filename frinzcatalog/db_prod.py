4ce622556312991dd6c22430cd40168cc04ccb28
Falensi Azmi <falensiazmi@gmail.com>
2024-06-04 01:48:36 +0700

    fix: change database credentials

--- frinzcatalog/db_prod.py
+++ frinzcatalog/db_prod.py
@@ -2,9 +2,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres', 
-        'USER': 'postgres', 
+        'USER': 'postgres.zrvktjuotsaqxzhidoiz', 
         'PASSWORD': 'mCfW8Gfb6yVdf477',
-        'HOST': 'db.zrvktjuotsaqxzhidoiz.supabase.co', 
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com', 
         'PORT': '5432',
     }
 }
